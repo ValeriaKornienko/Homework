2d77f5f8207ee48acda5e36d196ef0fc8c3a6371
valeb <valebog94@gmail.com>
2024-02-06 20:30:43 +0100

    New repository for Homework

---
+++ Homework24/HashMap/translation.java
@@ -0,0 +1,22 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class translation {
+
+    public static void main(String[] args) {
+        HashMap<String,String > translate = new HashMap<>();
+        translate.put("letter","письмо");
+        translate.put("book","книга");
+        translate.put("car","машина");
+        translate.put("circus","цирк");
+        translate.put("earth","земля");
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Задайте слово для перевода. Letter, book, car, circus, earth.");
+        String word = scanner1.nextLine();
+        System.out.println(translate.get(word));
+
+    }
+}
